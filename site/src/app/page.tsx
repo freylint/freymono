7420893b4966794a493f6a64a1e37e2140804063
freylint <lmpriestley@tuta.io>
2023-03-02 20:02:07 -0500

    Move freyground to site, converting to nextjs.

---
+++ site/src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from './page.module.css'
+
+
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}

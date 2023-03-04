1ebd0d4edb6d4da50a1e336841a32858881f135b
freylint <lmpriestley@tuta.io>
2023-03-03 21:45:41 -0500

    Apply tailwind theme to root of site.

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
         <link href="https://fonts.cdnfonts.com/css/helvetica-neue-5" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/fira_code.min.css" integrity="sha512-MbysAYimH1hH2xYzkkMHB6MqxBqfP0megxsCLknbYqHVwXTCg9IqHbk+ZP/vnhO8UEW6PaXAkKe2vQ+SWACxxA==" />
       </head>
-      <body>{children}</body>
+      <body className='font-sans'>{children}</body>
     </html>
   )
 }

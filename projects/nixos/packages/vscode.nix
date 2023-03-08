19a6eef08079337bdef69c156940c26b5847cb76
freylint <lmpriestley@tuta.io>
2023-03-07 19:21:39 -0500

    Move some plugins into managed, so I don't have to manage versions.

--- projects/nixos/packages/vscode.nix
+++ projects/nixos/packages/vscode.nix
@@ -13,38 +13,15 @@
         ms-azuretools.vscode-docker
         ms-vscode-remote.remote-ssh
         vscjava.vscode-java-debug
+        vscodevim.vim
+        github.vscode-pull-request-github
+        editorconfig.editorconfig
+        tamasfe.even-better-toml
+        vadimcn.vscode-lldb
+        github.copilot
+        redhat.java
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "vim";
-          publisher = "vscodevim";
-          version = "1.24.3";
-          sha256 = "e1f3e8441b6d59513c6777b83ba62b91fd3888ebbd125b294053f9ec738f5409";
-        }
-        {
-          name = "vscode-pull-request-github";
-          publisher = "GitHub";
-          version = "0.58.1";
-          sha256 = "22eb65e4926aaf085e72aed960d1c18888b37179c3dedad472b1e6b2068341cb";
-        }
-        {
-          name = "EditorConfig";
-          publisher = "EditorConfig";
-          version = "0.16.4";
-          sha256 = "8fe3f6a29ae91f4af3a88d152add096d91b5f440c4edeefe9006f73061824439";
-        }
         # General tooling
-        {
-          name = "even-better-toml";
-          publisher = "tamasfe";
-          version = "0.19.0";
-          sha256 = "32a4906ab35385b11fd701c34dfd72038e89321589378e9bd3c73bb55ebed675";
-        }
-        {
-          name = "vscode-lldb";
-          publisher = "vadimcn";
-          version = "1.8.1";
-          sha256 = "Uu7ESouPnScXs9x3iCb8MupSwXiZe5sSZqczpKA2aSQ=";
-        }
         {
           name = "vscodeintellicode";
           publisher = "VisualStudioExptTeam";
@@ -57,13 +34,6 @@
           publisher = "rust-lang";
           version = "0.4.1416";
           sha256 = "c6hRhXczpZwZV/49y373nZZh/5lonW5SdhRwxzFxPcU=";
-        }
-        # Java tooling
-        {
-          name = "java";
-          publisher = "redhat";
-          version = "1.15.0";
-          sha256 = "isYlOIx/P+6tkCAbjePBe2pIv5ORL9mkF2zxYXyArcM=";
         }
         # JS tooling
         {

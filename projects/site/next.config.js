646fa0408041bdf1b543ec04fd3af975bbf71c19
freylint <lmpriestley@outlook.com>
2023-03-15 19:25:52 -0400

    Remove exportPathMap.
    This is an exploratory change to see if it will fix JS loading issues related to JS loading after being deployed to azure.

--- projects/site/next.config.js
+++ projects/site/next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-  },
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    };
   }
 }
 

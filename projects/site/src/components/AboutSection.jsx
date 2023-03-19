e95f6f5c321571b2a1a04fb7130fe1010bc1c6c0
Lucas Priestley <lmpriestley@outlook.com>
2023-03-19 15:07:22 -0400

    Port AboutSection to rounded look.

--- projects/site/src/components/AboutSection.jsx
+++ projects/site/src/components/AboutSection.jsx
@@ -1,8 +1,8 @@
 export default function AboutSection({ title, children }) {
   return (
-    <section className="mx-2">
-      <h3>{title}</h3>
-      <p className="prose prose-lg text-white w-full ml-4">{children}</p>
+    <section className="px-2 rounded-md bg-slate-700">
+      <h3 className="text-xl text-white">{title}</h3>
+      <p className="w-full ml-4 prose prose-lg text-white">{children}</p>
     </section>
   );
 }

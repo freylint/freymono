7fdee7b7e87d47a1ad4a26bc29135ef5f72e960b
Lucas Priestley <lmpriestley@outlook.com>
2023-03-19 13:52:04 -0400

    Remove redundant main tag on headerbar.

--- projects/site/src/components/headerbar.tsx
+++ projects/site/src/components/headerbar.tsx
@@ -5,22 +5,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function HeaderBar() {
   return (
-    <main>
-      <div className='bg-gray flex flex-row text-white'>
-        <FGBranding />
-        <nav className='flex-row hidden md:flex m-3 grow'>
-          <p>Home</p>
-          <p>Posts</p>
-          <p>Projects</p>
-        </nav>
-        <div className='m-3 hidden md:flex flex-row min-w-fit'>
-          <HeaderBarIcon icon={faGithub} />
-          <HeaderBarIcon icon={faLinkedin} />
-          <HeaderBarIcon icon={faTwitter} />
-        </div>
-        <p className='m-3 md:hidden'>BURGER</p>
+    <div className='bg-gray flex flex-row text-white'>
+      <FGBranding />
+      <nav className='flex-row hidden md:flex m-3 grow'>
+        <p>Home</p>
+        <p>Posts</p>
+        <p>Projects</p>
+      </nav>
+      <div className='m-3 hidden md:flex flex-row min-w-fit'>
+        <HeaderBarIcon icon={faGithub} />
+        <HeaderBarIcon icon={faLinkedin} />
+        <HeaderBarIcon icon={faTwitter} />
       </div>
-    </main>
+      <p className='m-3 md:hidden'>BURGER</p>
+    </div>
   )
 }
 

c977822d71f649c574654e0f4cb7b055f6365c6b
freylint <lmpriestley@outlook.com>
2023-03-20 06:22:59 -0400

    Move blurbs to the blurbs section of the billboard.

--- projects/site/src/components/SplashPage.tsx
+++ projects/site/src/components/SplashPage.tsx
@@ -5,15 +5,17 @@ import BillBoard, {
 
 export default function SplashPage() {
   const marketingBlurb = "RAPID, ROBUST, ACCESSIBLE, ATTENTIVE";
+  const blogBlurb = "A tech blog that remembers humans";
+  const projectsBlurb = "Projects that make life easier";
+
+  const blurbs = [marketingBlurb, blogBlurb, projectsBlurb];
+
   return (
-    // TODO fetch header height from the theme
     <section className="flex flex-col h-screen bg-gradient-to-br from-indigo-800 to-pink-800 rounded-b-xl">
-      <p className="text-5xl text-center text-white py-7 grow mt-hbar">
-        Welcome to the Freyground
-      </p>
+      <p className="text-5xl text-center text-white py-7 grow mt-hbar"></p>
       <BillBoard groups={[]} />
       <h2 className="w-full my-4 text-2xl text-center text-white">
-        {marketingBlurb}
+        {blurbs[0]}
       </h2>
       <h2 className="mb-4 text-3xl text-center text-white">⌄</h2>
     </section>
